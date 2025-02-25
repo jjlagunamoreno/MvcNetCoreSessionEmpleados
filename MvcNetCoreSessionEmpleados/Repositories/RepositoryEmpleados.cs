2249cbdc611a4e2ca4f5fb9978f7811248278f38
jjlagunamoreno <jjlagunamoreno@gmail.com>
2025-02-25 10:20:04 +0100

    Add project files.

---
+++ MvcNetCoreSessionEmpleados/Repositories/RepositoryEmpleados.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcNetCoreSessionEmpleados.Data;
+using MvcNetCoreSessionEmpleados.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MvcNetCoreSessionEmpleados.Repositories
+{
+    public class RepositoryEmpleados
+    {
+        private HospitalContext context;
+
+        public RepositoryEmpleados(HospitalContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Empleado>> GetEmpleadosAsync()
+        {
+            return await this.context.Empleados.ToListAsync();
+        }
+
+        public async Task<Empleado> FindEmpleadoAsync(int idEmpleado)
+        {
+            return await this.context.Empleados
+                .FirstOrDefaultAsync(e => e.IdEmpleado == idEmpleado);
+        }
+    }
+}
